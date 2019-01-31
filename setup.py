a8511206ce325fb59644564e4231a8cb27727a66
joseph <joseph.chen@magnumwm.com>
2019-01-31 09:45:45 +0800

    UPDATE readme

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, find_packages
 
-
 setup(
 		name='ziplineaqm',
 		version='1.1',
@@ -11,5 +10,9 @@ setup(
 		packages=find_packages(),
 		install_requires=[
 			'numpy==1.16.0',
+			'pandas==0.22.0',
+			'pandas-datareader==0.7.0',
+			'trading-calendars==1.2.0',
+			'tabulate==0.8.3',
 		]
 )
